31485340d639d5821185f128c83cfe6ad7543ebc
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-09-20 14:14:00 -0600

    practicing different div formats

---
+++ app.js
@@ -0,0 +1,39 @@
+
+var td = document.querySelector("td")
+
+var h2 = document.querySelector("h2");
+
+
+td.addEventListener("click" , function(){
+    td.style.background = "blue";
+
+
+
+})
+
+// var newTd = document.createElement("td")
+// newTd.appendChild(pixelCanvas)
+
+
+
+// // var tr = document.querySelector("tr")
+
+// // pixelCanvas.eventListener ("click", function (){
+// var newTd = document.createElement("td");
+// tr.appendChild(newTd)
+    
+    
+    
+    // for (i=0; i < 10; i++) {
+        
+
+
+    //     }
+
+
+
+
+
+
+
+//})
