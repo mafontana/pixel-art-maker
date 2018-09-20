41f54f5b623c688f46dd89b766b9d11c27384088
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-09-20 14:27:38 -0600

    playing around with for loops changing pixel number

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 
 var td = document.querySelector("td")
 
+var tr = document.querySelector("tr")
+
 var h2 = document.querySelector("h2");
 
 
 td.addEventListener("click" , function(){
-    td.style.background = "blue";
-
-
-
+    // td.style.background = "blue";
+    
+    for (i=0; i < 10; i++) {
+    var newTd = document.createElement("td")
+    tr.appendChild(newTd)
+    }
 })
 
 // var newTd = document.createElement("td")
