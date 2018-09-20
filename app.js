2cf370be24d5251674a3a12946ec61a02c2ff9f0
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-09-20 15:31:07 -0600

    changed pixels to divs instead of table

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
+var square = document.querySelector("#square")
 
-var td = document.querySelector("td")
+// var  = document.querySelection("section")
 
-var tr = document.querySelector("tr")
-
-var h2 = document.querySelector("h2");
-
-
-td.addEventListener("click" , function(){
-    // td.style.background = "blue";
-    
-    for (i=0; i < 10; i++) {
-    var newTd = document.createElement("td")
-    tr.appendChild(newTd)
+for (i=0; i < 10; i++) {
+    var newSquare = document.createElement("div")
+    square.appendChild(newSquare);
     }
-})
+
+
+
+
+
+//Using Tables:
+
+// var td = document.querySelector("td")
+
+// var tr = document.querySelector("tr")
+
+// var h2 = document.querySelector("h2");
+
+// var table = document.querySelector("table")
+
+
+
+    
+    // for (i=0; i < 10; i++) {
+    // var newTd = document.createElement("td")
+    // tr.appendChild(newTd);
+    // var newTr = document.createElement("tr")
+    //     table.appendChild(newTr);
+    
+    // }
+
+    // for (i=0; i < 10; i++) {
+    //     var newTr = document.createElement("tr")
+    //     table.appendChild(newTr);
+    
+        
+    //     }
+
+
+
+// })
 
 // var newTd = document.createElement("td")
 // newTd.appendChild(pixelCanvas)
