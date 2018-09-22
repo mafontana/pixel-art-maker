5bf2783b4a48cb0fbbaf19d32b05a4f7b388c9c1
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-09-22 13:00:28 -0600

    pixel color now reacts to click and you can select a color with a color picker

--- app.js
+++ app.js
@@ -2,21 +2,35 @@ var square = document.querySelector(".square")
 
 var pixelCanvas = document.querySelector(".pixelCanvas")
 
+var color = document.querySelector("#colorPicker")
 
-
-for (i=0; i < 400; i++) {
-    var newSquare = document.createElement("div")
+for (i=0; i < 100; i++) {
+    let newSquare = document.createElement("div")
     newSquare.className = "square"
     pixelCanvas.appendChild(newSquare);
-    }
-    
 
-
-    // for (i=0; i < 400; i++) {
-        square.addEventListener("click", function(){
-        square.style.background = "blue"
+    newSquare.addEventListener("click", function(){
+        newSquare.style.background = color.value
     })
+}
 
 
+
+
+
+
+// var pixels = document.querySelectorAll("newSquare")
+    // for (i=0; i < 25; i++) {
+    // var pixels = document.querySelecto(".pixelCanvas")
+        
+    //     // var pixels = document.querySelector(".square")
+    //     newSquare.addEventListener("click", function(){
+    //     newSquare.style.background = "blue"
+    // })
+
+
+    //event object javascript event
+
+    
 
 
